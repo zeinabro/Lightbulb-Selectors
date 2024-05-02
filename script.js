df8f00b4726be7f37636a2a994b5b9e29ff93a61
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-02 09:45:23 +0100

    toggle lightbulb and count times clickes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Write your code here
+function toggleBulb(bulb){
+    bulb.classList.toggle("active");
+    count++;
+    subtitle.innerHTML = `You've clicked the lights ${count} time${count>1 ? "s":""}`;
+}
+
+let bulb1 = document.querySelector("#lightbulb1");
+let bulb2 = document.querySelector("#lightbulb2");
+let bulb3 = document.querySelector("#lightbulb3");
+let count = 0;
+let subtitle = document.querySelector(".subtitle")
+
+bulb1.addEventListener("click", () => {toggleBulb(bulb1)});
+bulb2.addEventListener("click", () => {toggleBulb(bulb2)});
+bulb3.addEventListener("click", () => {toggleBulb(bulb3)});
